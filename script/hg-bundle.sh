b9ec8da5c687c8a13a6727439056c7f5d97f77ad
ellis <ellis@rwest.io>
2021-08-14 18:48:12 -0500

    bundle script now creates bundle.tar.zst and moves to EXPORT_PATH

--- script/hg-bundle.sh
+++ script/hg-bundle.sh
@@ -2,8 +2,8 @@
 # run this from parent directory (~/src)
 CD=$(pwd)
 OUT=$HOME/stash/tmp/bundle
+EXPORT_PATH=/mnt/cia/export
 echo "Bundling all local repositories..."
-
 mkdir -p $OUT
 # Find all mercurial repositories, create bundles and dump them to $OUT dir
 for i in $(find . -name ".hg" | cut -c 3-); do
@@ -15,10 +15,15 @@ for i in $(find . -name ".hg" | cut -c 3-); do
 
     hg bundle -a -t gzip-v2 $OUT/$(basename $(hg root)).hg.gz
     hg bundle -a -t zstd-v2 $OUT/$(basename $(hg root)).hg.zst;
-    hg bundle -a -t none-v2 $OUT/$(basename $(hg root)).hg.tar;
+    hg bundle -a -t none-v2 $OUT/$(basename $(hg root)).hg;
     hg debugcreatestreamclonebundle $OUT/$(basename $(hg root)).hg.stream;
 
     cd $CD
 done
+
+pushd
+cd $HOME/stash/tmp
+tar --zstd -cf $EXPORT_PATH/bundle-$(date "+%Y%m%d-%H.%M").tar.zst bundle
+popd
 
 echo "Done."
