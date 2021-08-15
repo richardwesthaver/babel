25ac3e147b32b5ac7327ebedc2995ab71edffca7
ellis <ellis@rwest.io>
2021-08-15 01:46:37 -0500

    removed scratch repo, updated unbundle with unzstd for darwin

--- script/hg-unbundle.sh
+++ script/hg-unbundle.sh
@@ -6,6 +6,7 @@ BUNDLE_NAME=bundle-$(date "+%Y%m%d")
 PKG_DIR=$HOME/pkg/hg
 
 echo "unbundling $BUNDLE_NAME to $PKG_DIR"
+# the zstd options for tar no work for me, decompress archive (this should be MacOS only, maybe Win. need to add checks)
 unzstd $WD/$BUNDLE_NAME.tar.zst
 tar -xvf $WD/$BUNDLE_NAME.tar -C $WD/
 mv $WD/bundle/* $PKG_DIR/
