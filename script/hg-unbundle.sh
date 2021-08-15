3a20440caad08d291d4cdfe84be1e3b1574992f9
ellis <ellis@rwest.io>
2021-08-14 21:24:26 -0500

    update

--- script/hg-unbundle.sh
+++ script/hg-unbundle.sh
@@ -2,9 +2,11 @@
 # unbundle a tar.zst archive of Mercurial repositories.
 CD=$(pwd)
 WD=$HOME/stash/tmp
-BUNDLE_NAME=bundle-$(date "+%Y%m%d").tar.zst
+BUNDLE_NAME=bundle-$(date "+%Y%m%d")
 PKG_DIR=$HOME/pkg/hg
 
 echo "unbundling $BUNDLE_NAME to $PKG_DIR"
-tar -I 'zstd' -xvf $WD/$BUNDLE_NAME -C $WD/
+unzstd $WD/$BUNDLE_NAME.tar.zst
+tar -xvf $WD/$BUNDLE_NAME.tar -C $WD/
 mv $WD/bundle/* $PKG_DIR/
+rm -rf $WD/$BUNDLE_NAME.tar $WD/bundle
