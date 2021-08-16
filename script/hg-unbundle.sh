3c46f3ffee12e889e6cd8683953cbd9f91cceaa5
ellis <ellis@rwest.io>
2021-08-16 01:48:06 -0500

    more hg-unbundle edits

--- script/hg-unbundle.sh
+++ script/hg-unbundle.sh
@@ -5,9 +5,10 @@ WD=$HOME/stash/tmp
 BUNDLE_NAME=bundle-$(date "+%Y%m%d")
 PKG_DIR=$HOME/pkg/hg
 
-echo "unbundling $BUNDLE_NAME to $PKG_DIR"
+echo "unbundling $BUNDLE_NAME to $PKG_DIR/bundle"
 # the zstd options for tar no work for me, decompress archive (this should be MacOS only, maybe Win. need to add checks)
 unzstd $WD/$BUNDLE_NAME.tar.zst
 tar -xvf $WD/$BUNDLE_NAME.tar -C $WD/
 mv $WD/bundle $PKG_DIR/
 rm -rf $WD/$BUNDLE_NAME.tar $WD/bundle
+echo "Done."
