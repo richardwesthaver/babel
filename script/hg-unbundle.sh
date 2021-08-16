0e2b90c6abef657518e3cf5d4b6fe92a08f1cbaf
ellis <ellis@rwest.io>
2021-08-16 01:21:06 -0500

    forgot tar.zst in mv cmd

--- script/hg-unbundle.sh
+++ script/hg-unbundle.sh
@@ -9,5 +9,5 @@ echo "unbundling $BUNDLE_NAME to $PKG_DIR"
 # the zstd options for tar no work for me, decompress archive (this should be MacOS only, maybe Win. need to add checks)
 unzstd $WD/$BUNDLE_NAME.tar.zst
 tar -xvf $WD/$BUNDLE_NAME.tar -C $WD/
-mv $WD/$BUNDLE_NAME $PKG_DIR/
+mv $WD/$BUNDLE_NAME.tar.zst $PKG_DIR/
 rm -rf $WD/$BUNDLE_NAME.tar $WD/bundle
