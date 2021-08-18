5538578e7b85a3fd456fff1be8352ea1f69bc149
ellis <ellis@rwest.io>
2021-08-18 00:05:29 -0500

    tweak2

--- script/hg-unbundle.sh
+++ script/hg-unbundle.sh
@@ -6,7 +6,7 @@ BUNDLE_NAME=bundle-$(date "+%Y%m%d")*
 PKG_DIR=$HOME/pkg/hg
 
 for i in $(find $WD -name $BUNDLE_NAME); do
-    echo "unbundling $BUNDLE_NAME to $PKG_DIR/bundle"
+    echo "unbundling $i to $PKG_DIR/bundle"
     # the zstd options for tar no work for me, decompress archive (this should be MacOS only, maybe Win. need to add checks)
     unzstd $i.tar.zst
     tar -xvf $i.tar -C $PKG_DIR
