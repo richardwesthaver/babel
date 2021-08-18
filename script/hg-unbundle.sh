5016584655e982c2e4ab834cc313dd596d4721ea
ellis <ellis@rwest.io>
2021-08-18 00:09:31 -0500

    unbundle updates

--- script/hg-unbundle.sh
+++ script/hg-unbundle.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 # unbundle a tar.zst archive of Mercurial repositories.
-CD=$(pwd)
 WD=$HOME/stash/tmp
-BUNDLE_NAME=bundle-$(date "+%Y%m%d")*
+BUNDLE_NAME=bundle-$(date "+%Y%m%d")
 PKG_DIR=$HOME/pkg/hg
-
-for i in $(find $WD -name $BUNDLE_NAME); do
-    echo "unbundling $i to $PKG_DIR/bundle"
-    # the zstd options for tar no work for me, decompress archive (this should be MacOS only, maybe Win. need to add checks)
-    unzstd $i.tar.zst
-    tar -xvf $i.tar -C $PKG_DIR
-    rm -rf $i.tar.zst $i.tar
+echo "unbundling $i to $PKG_DIR/bundle"
+# the zstd options for tar no work for me, decompress archive (this should be MacOS only, maybe Win. need to add checks)
+unzstd $WD/$BUNDLE_NAME.tar.zst
+tar -xvf $WD/$BUNDLE_NAME.tar -C $PKG_DIR
+rm -rf $WD/$BUNDLE_NAME.tar.zst $WD/$BUNDLE_NAME.tar
 echo "Done."
 done
