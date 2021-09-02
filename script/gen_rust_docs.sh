8eb19896ce99e63474587bf0dc113d258cacfcc5
ellis <ellis@rwest.io>
2021-09-02 04:45:20 -0500

    edited gen_rust_docs script

--- script/gen_rust_docs.sh
+++ script/gen_rust_docs.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# generate inline documentation for all rust projects
+# generate inline documentation for my rust libraries
 set -eu
 
-WD=~/src
+WD=/mnt/src
 
 pushd $WD
-for i in demo store stash rlib tenex; do # shed
+for i in rlib tenex; do # shed
     echo "";
     echo "generating rust_docs for $i";
     cd "$i";
-    cargo +nightly doc --target-dir ~/stash/tmp/rust_docs;
+    cargo +nightly doc --target-dir ~/stash/tmp/rust_docs/$i --all-features --workspace --no-deps;
     cd $WD;
 done
 popd
