f08097c4f53d73096519e48013446a148687d9e3
ellis <ellis@rwest.io>
2021-11-17 00:43:58 -0500

    readme.org update

--- sc/hg-export-github.sh
+++ sc/hg-export-github.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # export REPO to git REMOTE
-REPO=shed
+REPO=meta
 SRC=$SHED/src/$REPO
 REMOTE=git@github.com:richardwesthaver/$REPO.git
 WD=$STAMP
@@ -8,8 +8,8 @@ WD=$STAMP
 rm -rf $WD/$REPO
 git clone $REMOTE $WD/$REPO
 pushd $WD/$REPO
-cp -rf $SRC/{Cargo.toml,build.rs,lisp,makefile,src,readme.org,rustfmt.toml} ./ 
-#cp -rf $SRC/{m.org,n.org,cv.org,index.org,ox.setup,readme.org} ./
+#cp -rf $SRC/{Cargo.toml,build.rs,lisp,makefile,src,readme.org,rustfmt.toml} ./ 
+cp -rf $SRC/{m.org,n.org,cv.org,index.org,ox.setup,readme.org} ./
 #cp -rf $SRC/{babel.el,lob.org,readme.org,makefile,sc} ./
 #cp -rf $SRC/{Cargo.toml,.cargo,rustfmt.toml,src,alch,audio,crypto,db,eve,flate,fu,hash,kala,logger,math,net,obj,organ,tests,ui,util,readme.org} ./
 #cp -rf $SRC/{emacs,mail,shell,term,tmux,vc,virt,wm,readme.org} ./
