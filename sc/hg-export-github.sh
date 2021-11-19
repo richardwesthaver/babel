bd3e4f60ee8a0076acf3b502120e9c0fec7a3669
ellis <ellis@rwest.io>
2021-11-19 04:26:51 -0500

    readme.org update

--- sc/hg-export-github.sh
+++ sc/hg-export-github.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# export REPO to git REMOTE
+# export REPO to github mirror
 REPO=meta
 SRC=$SHED/src/$REPO
 REMOTE=git@github.com:richardwesthaver/$REPO.git
@@ -7,13 +7,18 @@ WD=$STAMP
 
 rm -rf $WD/$REPO
 git clone $REMOTE $WD/$REPO
+
 pushd $WD/$REPO
-#cp -rf $SRC/{Cargo.toml,build.rs,lisp,makefile,src,readme.org,rustfmt.toml} ./ 
-cp -rf $SRC/{m.org,n.org,cv.org,index.org,ox.setup,readme.org} ./
-#cp -rf $SRC/{babel.el,lob.org,readme.org,makefile,sc} ./
-#cp -rf $SRC/{Cargo.toml,.cargo,rustfmt.toml,src,alch,audio,crypto,db,eve,flate,fu,hash,kala,logger,math,net,obj,organ,tests,ui,util,readme.org} ./
-#cp -rf $SRC/{emacs,mail,shell,term,tmux,vc,virt,wm,readme.org} ./
+
+case $REPO in
+    meta) cp -rf $SRC/{m.org,n.org,cv.org,index.org,ox.setup,readme.org} ./ ;;
+    shed) cp -rf $SRC/{Cargo.toml,build.rs,lisp,makefile,src,readme.org,rustfmt.toml} ./ ;;
+    babel) cp -rf $SRC/{babel.el,lob.org,readme.org,makefile,sc} ./ ;;
+    rlib) cp -rf $SRC/{Cargo.toml,.cargo,rustfmt.toml,src,alch,audio,crypto,db,eve,flate,fu,hash,kala,logger,math,net,obj,organ,tests,ui,util,readme.org} ./ ;;
+    cfg) cp -rf $SRC/{emacs,mail,shell,term,tmux,vc,virt,wm,readme.org} ./ ;;
+    tenex) cp -rf $SRC/{src,aws,google,readme.org,rustfmt.toml,Cargo.toml} ./ ;;
+esac
 git add .
-git commit -m "mirror export $(date '+%Y%m%d') from hg.rwest.io/$REPO"
+git commit -m "from https://hg.rwest.io/$REPO"
 git push
 popd
