f54d288b8c424d05f7d7f8cce91dbbfe5dab1692
ellis <ellis@rwest.io>
2022-08-13 03:23:29 -0400

    fast export

--- sh/hg-fast-export.sh
+++ sh/hg-fast-export.sh
@@ -4,10 +4,10 @@ FE=~/stash/fast-export/hg-fast-export.sh
 NAME=$(basename $CD)
 git init /tmp/$NAME
 pushd /tmp/$NAME
-git config core.ignoreCase false && git config push.followTags true
+git config core.ignoreCase false && git config push.followTags true &&
 # '-M default' = no more masters
 $FE -r $CD -M default && git checkout HEAD
 git remote add gh git@github.com:richardwesthaver/$NAME.git
-git push gh --all
+git push gh --all --force
 popd
 rm -rf /tmp/$NAME
