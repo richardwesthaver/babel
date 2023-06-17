7b0c2c8126c58d5a0f7cd74ce6c23a4944b1db8a
ellis <ellis@rwest.io>
2023-06-16 22:20:09 -0400

    fixed fast-export

--- sh/hg-fast-export.sh
+++ sh/hg-fast-export.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 CD=$PWD
-FE=PYTHON=python3.11 ~/stash/fast-export/hg-fast-export.sh
+FE=~/stash/fast-export/hg-fast-export.sh
 NAME=$(basename $CD)
 git init /tmp/$NAME
 pushd /tmp/$NAME
 git config core.ignoreCase false && git config push.followTags true &&
 # '-M default' = no more masters
-$FE -r $CD -M default && git checkout HEAD
+PYTHON=python3.11 $FE -r $CD -M default && git checkout HEAD
 git remote add gh git@github.com:richardwesthaver/$NAME.git
 git push gh --all --force
 popd
