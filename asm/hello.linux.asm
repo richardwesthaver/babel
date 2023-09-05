43bfe26cccb8cfcfad3ab7c4ad974ea0bd215f73
ellis <ellis@rwest.io>
2023-09-04 23:01:44 -0400

    asm stuff

---
+++ asm/hello.linux.asm
@@ -0,0 +1,23 @@
+;;; asm/hello.linux.asm --- nasm example
+
+;; nasm -f elf hello.linux.asm && ld -m elf_x86_64 hello.linux.o -o hello.linux && hello.linux
+
+;;; Code:
+global _start
+
+section .data
+msg:	db	"Hello, world!", 10
+.len:	equ	$ - msg
+
+section .text
+_start:
+	mov	eax, 4 ; write
+	mov	ebx, 1 ; stdout
+	mov	ecx, msg
+	mov	edx, msg.len
+	int	0x80   ; write(stdout, msg, strlen(msg));
+
+	xor	eax, msg.len ; invert return value from write()
+	xchg eax, ebx ; value for exit()
+	mov	eax, 1 ; exit
+	int	0x80   ; exit(...)
