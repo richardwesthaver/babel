8318494178c368f696253178dd5eda57dabf589b
ellis <ellis@rwest.io>
2023-12-01 23:20:27 -0500

    t-rec tweaks

--- sh/record-gif.sh
+++ sh/record-gif.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/bash
 # Wrapper for t-rec program
-t-rec -l
-read -p "WINDOWID = " recwin
-WINDOWID=$recwin t-rec -d none -b black -mqn -s 1s -e 2s
+WINDOWID="${1}" t-rec -b transparent -qn -s 800ms -e 800ms --output "${2:-$(date +%s)}"
